d0266009654b520391a749039b1d046734e278fe
Nathaniel Chien <nchien2@stanford.edu>
2022-02-10 01:35:15 -0800

    Fixed bugs with login flow. Added post creation, post display pages, and profile page.

--- wellbi/profile.py
+++ wellbi/profile.py
@@ -1,6 +1,6 @@
 import functools
 from . import db_endpoints, __init__
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, Recaptcha, RecaptchaField
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 from flask import current_app as app
@@ -61,7 +61,8 @@ class loginForm(FlaskForm):
 class signupForm(FlaskForm):
     uname = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = StringField('Confirm Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    recaptcha = RecaptchaField(validators=[Recaptcha(message="Recaptcha failed. Try again.")])
     submit = SubmitField('Sign up')
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -74,6 +75,9 @@ def login():
         if not id:
             flash('Username ' + username + ' not found. Try again.')
             return redirect(url_for('profile.login', type='login'))
+        if password != user_dict['password']:
+            flash('Invalid password.')
+            return render_template('login.html', form=form, type='login')
         logged_user = User(username)
         flask_login.login_user(logged_user, remember=True)
         print(flask_login.current_user.is_authenticated())
@@ -96,6 +100,9 @@ def signup():
         if id:
             flash('Email address already exists.')
             return redirect(url_for('profile.signup', type='signup'))
+        
+        logged_user = User(username)
+        flask_login.login_user(logged_user, remember=True)
         return redirect(url_for('profile.display', username=username))
     return render_template('login.html', form=form, type='signup')
 
@@ -103,7 +110,20 @@ def signup():
 @bp.route('/', methods=('GET', 'POST'))
 @login_required
 def display():
-    return render_template('profile.html', username=flask_login.current_user.username)
+    username = flask_login.current_user.username
+    user_obj = db_endpoints.get_user_by_id(username)
+    posts = user_obj.to_dict()['posts']
+    print(posts)
+
+    title_list =[]
+    id_list = []
+    for post_ref in posts:
+        post = post_ref.get()
+        title_list.append(post.to_dict()['title'])
+        id_list.append(post.id)
+    print(id_list)
+    print(title_list)
+    return render_template('profile.html', username=username, title_list=title_list, id_list=id_list)
 
 @bp.route('/logout', methods=['GET'])
 @login_required
