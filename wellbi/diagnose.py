d4603b3cc360b6cfa98946390f698b23590870be
Nandini Naidu <nnaidu@stanford.edu>
2022-03-05 21:09:16 -0800

    implemented map API key, added zipcode input

--- wellbi/diagnose.py
+++ wellbi/diagnose.py
@@ -9,6 +9,7 @@ from flask import (
 
 bp = Blueprint('diagnose', __name__, url_prefix='/diagnose')
 
+
 # Display forum here
 @bp.route('/forums', methods=('GET', 'POST'))
 def forums():
@@ -17,7 +18,7 @@ def forums():
 # Display resources here
 @bp.route('/resources', methods=('GET', 'POST'))
 def resources():
-    return render_template("resources.html")
+    return redirect("/resources/clinics")
 
 # Display results here
 @bp.route('/results', methods=('GET', 'POST'))
@@ -39,17 +40,20 @@ def results():
         for index, row, in df.iterrows():
             disease_match[row["Disease"]] = 0
 
-        found_symptom = False
         for symptom in all_symptoms:
             checked = request.form.get(symptom)
             if checked:
-                found_symptom = True
                 for index, row in df.iterrows():
                     symptoms = row["Symptoms"]
                     if symptom in symptoms:
                         disease_match[row["Disease"]] += 1
-                        found_symptom = True
 
+    sorted_disease_match = sorted(disease_match.items(), key = operator.itemgetter(1), reverse=True)
+
+
+    curr_user_path=Path(__file__).parent.absolute()
+    diagnosis = sorted_disease_match[0][0]
+    common_symptoms = ""
     html = """
     {% extends 'base.html' %}
 
@@ -59,66 +63,6 @@ def results():
 
     {% block content %}
     """
-<<<<<<< HEAD
-    if found_symptom == False:
-        html += """
-        <p> You didn't check any symptoms! Please restart the diagnosis process.  </p>
-        """
-    else:
-
-
-        sorted_disease_match = sorted(disease_match.items(), key = operator.itemgetter(1), reverse=True)
-
-
-        curr_user_path=Path(__file__).parent.absolute()
-        diagnosis = sorted_disease_match[0][0]
-        common_symptoms = ""
-
-        html += f"""
-          <p> Please note: The results may not include your complete medical history and are provided solely for informational purposes and is not a substitute for professional medical advice. 
-          </p>
-          <div class = "results">
-            <p><b> </b>From the symptoms you inputted it appears you may have {diagnosis}! 
-            </p>
-            <p> {diagnosis} is common in both men and women.
-            </p>
-            <div class = "recommendation">
-              <p> We recommend you make an appointment to see a medical professional.
-              </p>
-              <p><a href="{"{{"} url_for('diagnose.resources'){"}}"}">Find Resources on {diagnosis}</a>
-              </p>
-            </div>
-            <div class="Feedback">
-              <h3 class="header">Feedback</h3>
-              <p> Was this information helpful?
-              </p>
-              <head>
-              <meta charset="UTF-8">
-              <link rel="stylesheet" type="text/css" href="style.css">
-              <title>Star Rating</title>
-              </head>
-        
-              <body>
-              <div class="rate">
-                <input type="radio" id="star5" name="rate" value="5" />
-                <label for="star5" title="text">5 stars</label>
-                <input type="radio" id="star4" name="rate" value="4" />
-                <label for="star4" title="text">4 stars</label>
-                <input type="radio" id="star3" name="rate" value="3" />
-                <label for="star3" title="text">3 stars</label>
-                <input type="radio" id="star2" name="rate" value="2" />
-                <label for="star2" title="text">2 stars</label>
-                <input type="radio" id="star1" name="rate" value="1" />
-                <label for="star1" title="text">1 star</label>
-              </div>
-              </body>
-            </div>
-            <div class="Forums">
-              <h4 class="header">Forums</h4>
-              <p><a href="{"{{"} url_for('diagnose.forums'){"}}"}">Connect with Others</a>
-              </p>
-            </div>
-=======
     html += f"""
       <p> Please note: The results may not include your complete medical history and are provided solely for informational purposes and is not a substitute for professional medical advice.
       </p>
@@ -155,9 +99,16 @@ def results():
             <label for="star2" title="text">2 stars</label>
             <input type="radio" id="star1" name="rate" value="1" />
             <label for="star1" title="text">1 star</label>
->>>>>>> Ryan
           </div>
-        """
+          </body>
+        </div>
+        <div class="Forums">
+          <h4 class="header">Forums</h4>
+          <p><a href="{"{{"} url_for('diagnose.forums'){"}}"}">Connect with Others</a>
+          </p>
+        </div>
+      </div>
+    """
     html += """
     {% endblock %}
     """
--- wellbi/resources.py
+++ wellbi/resources.py
@@ -1,15 +1,63 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+import pgeocode
+import geopy.distance
+import heapq
 
 bp = Blueprint('resources', __name__, url_prefix='/resources')
 
 
+class getclinicsForm(FlaskForm):
+    zipcode = StringField('Zipcode', validators=[DataRequired(), Length(5)])
+    submit = SubmitField('Search')
 
 # Display diagnosis here
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/clinics', methods=('GET', 'POST'))
 def clinics():
-    return render_template("resources.html")
+    clinics = [['Castro-Mission Health Center', {'lat': 37.7631636584137, 'lng': -122.43202042310821}],
+                        ['Lyon-Martin Women’s Health Services', {'lat': 37.769627008041525, 'lng': -122.4198499752374}],
+                        ['Tom Waddell Health Center', {'lat': 37.779073814363635, 'lng': -122.41836942751574}],
+                        ['Haight-Ashbury Free Medical Clinic', {'lat': 37.771016131782325, 'lng': -122.44843816450916}],
+                        ['Mission Neighborhood Health Center', {'lat': 37.765220269982855, 'lng': -122.41662428341466}],
+                        ['Southeast Health Center', {'lat': 37.7261632575225, 'lng': -122.39228857523872}],
+                        ['Chinatown Public Health Center', {'lat': 37.79745393449752, 'lng': -122.41150583106013}],
+                        ['North East Medical Services', {'lat': 37.800108351119306, 'lng': -122.40896287893071}],
+                        ['Cole Street Youth Clinic', {'lat': 37.770099563710545, 'lng': -122.45043984455516}],
+                        ['Sister Mary Philippa Health Center', {'lat': 37.773146543209776, 'lng': -122.45307070776671}],
+                        ['Childrens Health Center at SFGH', {'lat': 37.756585087094145, 'lng': -122.40427153925992}],
+                        ['Family Health Center at SFGH', {'lat': 37.75750031858322, 'lng': -122.40611502751149}],
+                        ['Native American Health Center', {'lat': 37.76424155557056, 'lng': -122.41870581571993}],
+                        ['Potrero Hill Health Center', {'lat': 37.754232430120844, 'lng': -122.39893124270846}],
+                        ['Women’s Health Center at SFGH', {'lat': 37.754779729781255, 'lng': -122.40567247282607}],
+                        ['San Francisco Free Clinic', {'lat': 37.78493320501721, 'lng': -122.47006635804873}],
+                        ['Hip Hop to Health', {'lat': 37.714599013912654, 'lng': -122.46688415989787}],
+                        ['South of Market Health Center', {'lat': 37.77967993403819, 'lng': -122.40940407523719}],
+                        ['Ocean-Park Health Center', {'lat': 37.7624928425454, 'lng': -122.48262381571989}],
+                        ['Larkin Street Youth Clinic', {'lat': 37.7882815769076, 'lng': -122.4192406733899}],
+                        ['Curry Senior Center', {'lat': 37.782823847673576, 'lng': -122.4145124598959}],
+                        ['St. Anthony Free Medical Clinic', {'lat': 37.78241964606527, 'lng': -122.41320307339001}],
+                        ['Silver Avenue Family Health Center', {'lat': 37.732595039016246, 'lng': -122.40644320222667}],
+                        ['Maxine Hall Health Center', {'lat': 37.779490497968744, 'lng': -122.42917569242543}]]
+    #print("here")
+    form = getclinicsForm()
+    if form.validate_on_submit():
+        zip = form.zipcode.data
+        nomi = pgeocode.Nominatim('us')
+        user_loc = nomi.query_postal_code(zip)
+        user_coords = (user_loc['latitude'], user_loc['longitude'])
+        closest = {}
+        for i in range(len(clinics)): #carry out calculations to actually find closest clinics
+            clinic_coords = (clinics[i][1]['lat'], clinics[i][1]['lng'])
+            distance = geopy.distance.geodesic(user_coords, clinic_coords).miles
+            closest[clinics[i][0]]= distance
+        closest = heapq.nsmallest(5,closest,key=lambda x: (closest.get(x),x))
+        return render_template("resources.html", form=form, zipcode=zip, clinics=closest)
+    return render_template("resources.html", form=form)
+
 
 @bp.route('/chlamydia', methods=('GET', 'POST'))
 def chlamydia():
